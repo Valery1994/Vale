b452ed69f88d35c0b9762f13a780ddd474c74a46
Valery1994 <102192186+Valery1994@users.noreply.github.com>
2022-03-29 20:33:14 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,9 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+btnEnv.addEventListener("click"),
+  () => {
+    let base: number = 3;
+    let altura: number = 1;
+    let area: number = base * altura;
+  };
